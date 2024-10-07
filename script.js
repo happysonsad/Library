ddac2babc5f5e77430e7f65d3cf01d906acee436
happysonsad <176979803+happysonsad@users.noreply.github.com>
2024-10-07 13:56:51 +0800

    finished the javascript part

--- script.js
+++ script.js
@@ -1,33 +1,94 @@
 const myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
-    };
+    this.read = read;
+    this.bookInfo = function () {
+        message = `${this.title} by ${this.author}, ${this.pages} pages`;
+        if (this.read) {
+            message += ", read";
+        } else {
+            message += ", not yet read";
+        } return message;
+    }
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#status").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    updateLilbraryDisplay();
+    document.querySelector("#bookForm").reset();
+    addBookModal.style.display = "none";
 }
 
+function updateLilbraryDisplay() {
+    const Library = document.querySelector("#booksGrid");
+    Library.textContent = "";
 
-// DOM
-const addBookModal = document.querySelector("#addBookModal");
+    myLibrary.forEach((book, index) => {
+        const BookCard = document.createElement("div");
+        BookCard.classList.add("book-card");
 
+        const bookInfo = document.createElement("div");
+        bookInfo.textContent = book.bookInfo();
+
+        // const bookTitle = document.createElement("p");
+        // bookTitle.classList.add("bookTitle");
+        // bookTitle.textContent = this.title.value;
+
+        // const bookAuthor = document.createElement("p");
+        // bookAuthor.classList.add("bookAuthor");
+        // bookAuthor.textContent = this.author.value;
+
+        // const bookPages = document.createElement("p");
+        // bookPages.classList.add("bookPages");
+        // bookPages.textContent = this.pages.value;
+
+        const bookRead = document.createElement("button")
+        bookRead.textContent = book.read ? "Read" : "Not Read";
+        bookRead.classList.add("readStatus");
+        bookRead.addEventListener("click", () => {
+            book.read = !book.read;
+            updateLilbraryDisplay();
+        })
+
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("removeButton");
+        removeButton.addEventListener("click", () => {
+            myLibrary.splice(index, 1);
+            updateLilbraryDisplay();
+        })
+
+        BookCard.appendChild(bookInfo);
+
+        // BookCard.appendChild(bookTitle);
+        // BookCard.appendChild(bookAuthor);
+        // BookCard.appendChild(bookPages);
+
+        BookCard.appendChild(bookRead);
+        BookCard.appendChild(removeButton);
+
+        Library.appendChild(BookCard);
+    })
+}
+
+document.querySelector("#bookForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+    console.log(myLibrary);
+})
+
+// Modal
 const addBookButton = document.querySelector(".addBookButton");
-
-const submitButton = document.querySelector(".submitButton");
 
 addBookButton.addEventListener("click", function() {
     addBookModal.style.display = "flex";
-})
-
-submitButton.addEventListener("click", function() {
-    addBookModal.style.display = "none";
 })
 
 window.addEventListener("click", function(event) {
